60a58c8eab4f76edacec4178215aeed96478214f
VASILEIOS SKLIRIS <vasileios.skliris@ligo.org>
2023-01-29 13:07:08 -0800

    Comment comand to get condor_id

--- run-tritonserver.py
+++ run-tritonserver.py
@@ -31,19 +31,9 @@ job = Job(name="run-tritonserver",
                       "+SingularityImage = \"/cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12\""]
           )
 
-# job = Job(name="run-tritonser
-#           requirements="'HasSingularity'",
-#           executable="/opt/tritonserver/bin/tritonserver",
-#         #   executable="apptainer run /cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12",
-#           error=condor_dir,
-#           log=condor_dir,
-#           output=condor_dir,
-#           extra_lines=["accounting_group_user="+os.environ['HOME'].split("/")[-1],
-#                        "accounting_group=allsky.mlyonline",
-#                        "request_disk=64M",
-#                        "+SingularityImage=/cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12"])
-
-
-job.build_submit(fancyname=True)
+job.add_arg("--model-repository /home/vasileios.skliris/testhermes")
+# hot to get condor_id
+# var=$(echo -e $(python run-tritonserver.py) | grep "submitted" | awk '{pint $8}' | tr -d '.')
+job.build_submit()
 
 
