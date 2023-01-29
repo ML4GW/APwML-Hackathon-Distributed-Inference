9c3b33d5fb48f49a52db54f072e49d5ad8ff2c7e
VASILEIOS SKLIRIS <vasileios.skliris@ligo.org>
2023-01-29 12:12:51 -0800

    condor_submit with correct format and gitignore
    run-triton-server.py needs a model directory as argument to run properly

--- run-tritonserver.py
+++ run-tritonserver.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 
 import os
-from pycondor import Job
+from pycondor import Job, Dagman
 
 # apptainer run /cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12
 
-condor_dir = "condor_run-tritonserver/"
+error = 'condor/error'
+output = 'condor/output'
+log = 'condor/log'
+submit = 'condor/submit'
+
 
 job = Job(name="run-tritonserver",
-          executable="sleep",
-        #   executable="apptainer run /cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12",
-          error=condor_dir,
-          log=condor_dir,
-          output=condor_dir,
+          executable="/opt/tritonserver/bin/tritonserver",
+          submit=submit,
+          error=error,
+          output=output,
+          log=log,
+          requirements="HasSingularity",
+
+          
           extra_lines=["accounting_group_user="+os.environ['HOME'].split("/")[-1],
-                       "accounting_group=allsky.mlyonline",
-                       "request_disk=64M"])
+                       "accounting_group=ligo.dev.o4.burst.allsky.mlyonline",
+                       "request_disk=64M",
+                       "request_GPUs = 1",
+                       "request_cpus = 8",
+                       "transfer_executable = False",
+                      # "HasSingularity=True",
+                       #"SingularityVersion=\"/cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12\""])
+                      "+SingularityImage = \"/cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12\""]
+          )
 
-job.add_arg('100')
-
-# job = Job(name="run-tritonserver",
+# job = Job(name="run-tritonser
 #           requirements="'HasSingularity'",
 #           executable="/opt/tritonserver/bin/tritonserver",
 #         #   executable="apptainer run /cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12",
@@ -31,4 +43,7 @@ job.add_arg('100')
 #                        "request_disk=64M",
 #                        "+SingularityImage=/cvmfs/singularity.opensciencegrid.org/ml4gw/hermes/tritonserver:22.12"])
 
-job.build_submit()
+
+job.build_submit(fancyname=True)
+
+
